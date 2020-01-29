10cebb972d31d591b849441fa7642845f87537ca
Elliott Record <elliottrecord@protonmail.com>
2020-01-29 22:06:47 +0000

    Added expanded by default support.

--- PrismOutlook.Business/NavigationItem.cs
+++ PrismOutlook.Business/NavigationItem.cs
@@ -6,7 +6,7 @@ namespace PrismOutlook.Business
     {
         public string Caption { get; set; }
         public string NavigationPath { get; set; }
-
+        public bool IsExpanded { get; set; }
         public ObservableCollection<NavigationItem> Items { get; set; }
 
         public NavigationItem()
