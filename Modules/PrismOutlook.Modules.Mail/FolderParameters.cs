005107aa1b4496cc8eb13891fe59e2b2fe83878f
Elliott Record <elliottrecord@protonmail.com>
2020-02-03 20:41:36 +0000

    Refactored FolderParameter into its own file.

---
+++ Modules/PrismOutlook.Modules.Mail/FolderParameters.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrismOutlook.Modules.Mail
+{
+    public class FolderParameters
+    {
+        public const string FolderKey = "Folder";
+
+        public const string Inbox = "Inbox";
+        public const string Sent = "Sent";
+        public const string Deleted = "Deleted";
+
+    }
+}
